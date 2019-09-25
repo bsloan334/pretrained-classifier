919c76d09b62c7f88789bb0652b646a789368599
SLOAN <rs833t@att.com>
2019-09-25 09:06:06 -0400

    updating changes

--- classify_images.py
+++ classify_images.py
@@ -70,7 +70,7 @@ def classify_images(images_dir, results_dic, model):
         model_label = classifier(str(images_dir) + key, model).strip().lower()
         truth = results_dic[key][0]
     
-    if truth in model_label:
-        results_dic.extend([model_label, 1])
-    else:
-        results_dic.extend([model_label, 0])
+      if truth in model_label:
+          results_dic[model_label] = 1
+      else:
+          results_dic[model_label] = 0
