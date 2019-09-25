0f96f7c76ad3666858207f9c31f329629f83944c
SLOAN <rs833t@att.com>
2019-09-25 09:43:56 -0400

    fixing the classify images

--- classify_images.py
+++ classify_images.py
@@ -71,6 +71,6 @@ def classify_images(images_dir, results_dic, model):
         truth = results_dic[key][0]
     
       if truth in model_label:
-          results_dic[model_label] = 1
+          results_dic[key].extend((model_label, 1))
       else:
-          results_dic[model_label] = 0
+          results_dic[key].extend((model_label, 0))
